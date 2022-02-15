80036b2c9a08858df54d4742d9b00b673f1432e6
Kristaps Ritins <kritinsdev@gmail.com>
2022-02-16 00:32:33 +0200

    game logic implementation

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let gameScore = 20;
 const checkButton = document.querySelector('.check');
 
-checkButton.addEventListener("click", function() {
-    console.log('checked');
+document.querySelector('.number').textContent = secretNumber;
+
+checkButton.addEventListener("click", function () {
+    const infoText = document.querySelector('.message');
+    const guessValue = Number(document.querySelector('.guess').value);
+
+
+    if (!guessValue) {
+        infoText.textContent = '⛔ Add valid number!';
+    } else if (guessValue === secretNumber) {
+        infoText.textContent = '🏆 You guessed correct!'
+    } else if (guessValue > secretNumber) {
+        if (gameScore > 1) {
+            gameScore--;
+            infoText.textContent = 'Number is too high!';
+            document.querySelector('.score').textContent = gameScore;
+        } else {
+            infoText.textContent = "You've lost the game!";
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guessValue < secretNumber) {
+        if (gameScore > 1) {
+            gameScore--;
+            infoText.textContent = 'Number is too low!';
+            document.querySelector('.score').textContent = gameScore;
+        } else {
+            infoText.textContent = "You've lost the game!";
+            document.querySelector('.score').textContent = 0;
+        }
+    }
 })
