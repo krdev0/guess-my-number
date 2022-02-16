1021726506de75957f91764026b7916d69d0440d
Kristaps Ritins <kritinsdev@gmail.com>
2022-02-16 23:26:44 +0200

    refactored code + finished app

--- script.js
+++ script.js
@@ -2,58 +2,56 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let gameScore = 20;
+let highscore = 0;
 const checkButton = document.querySelector('.check');
 const playAgainButton = document.querySelector('.again');
-let gameWon = false;
+
+const displayMessage = function(message) {
+    document.querySelector(".message").textContent = message;
+}
 
 checkButton.addEventListener("click", function () {
-    const infoText = document.querySelector('.message');
     const guessValue = Number(document.querySelector('.guess').value);
+    
     if (!guessValue) {
         //When there is no valid input
-        infoText.textContent = '⛔ Add valid number!';
+        displayMessage('⛔ Add valid number!');
 
     } else if (guessValue === secretNumber) {
         //When user guesses right number
-        infoText.textContent = '🏆 You guessed correct!';
+        displayMessage('🏆 You guessed correct!');
         document.querySelector("body").style.backgroundColor = '#60b347';
         document.querySelector(".number").style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
-        gameWon = true;
         checkButton.disabled = true;
-    } else if (guessValue > secretNumber && !gameWon) {
-         //When user guesses number thats larger than secret number
+
+        if (gameScore > highscore) {
+            highscore = gameScore;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+
+    } else if (guessValue !== secretNumber) {
+
         if (gameScore > 1) {
             gameScore--;
-            infoText.textContent = 'Number is too high!';
+            displayMessage((guessValue > secretNumber) ? 'Number is too high!' : 'Number is too low!');
             document.querySelector('.score').textContent = gameScore;
         } else {
-            infoText.textContent = "You've lost the game!";
+            displayMessage("You've lost the game!");
             document.querySelector('.score').textContent = 0;
         }
 
-    } else if (guessValue < secretNumber && !gameWon) {
-        //When user guesses number thats smaller than secret number
-        if (gameScore > 1) {
-            gameScore--;
-            infoText.textContent = 'Number is too low!';
-            document.querySelector('.score').textContent = gameScore;
-        } else {
-            infoText.textContent = "You've lost the game!";
-            document.querySelector('.score').textContent = 0;
-        }
     }
 });
 
-playAgainButton.addEventListener("click", function() {
-    gameScore = 20; 
+playAgainButton.addEventListener("click", function () {
+    gameScore = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector(".message").textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector(".number").textContent = '?';
     document.querySelector(".score").textContent = 20;
     document.querySelector("body").style.backgroundColor = "#222";
     document.querySelector(".number").style.width = "15rem";
     document.querySelector(".guess").value = '';
     checkButton.disabled = false;
-    gameWon = false;
 });
